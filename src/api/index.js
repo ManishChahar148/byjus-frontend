8b2922525c6f3afcf21cc247468916ad07823262
ManishChahar7523 <manishchahar@myanatomy.in>
2022-03-06 23:45:06 +0530

    url update

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:7000'
+const baseUrl = 'https://byjus-backend-manish.herokuapp.com'
 
 export const getAllUsers = () => {
     return axios.get(baseUrl + '/all-users');
